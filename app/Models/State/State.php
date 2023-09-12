46836af8eaaa89e3f3f38b9b31e1f7b37b4148f3
JUANCAMILO2001 <juan.camilorodrigez@gmail.com>
2023-09-11 20:36:32 -0500

    Modelo de estado

--- app/Models/State/State.php
+++ app/Models/State/State.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class State extends Model
 {
     use HasFactory;
+    protected $table = 'states';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+      'name',
+      'check',
+    ];
 }
