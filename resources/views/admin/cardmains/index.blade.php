eb8721d3db1cb14d4e4ce01a516486b870d52a65
JUANCAMILO2001 <juan.camilorodrigez@gmail.com>
2023-09-15 21:16:43 -0500

    Configuracion en noticias generales

--- resources/views/admin/cardmains/index.blade.php
+++ resources/views/admin/cardmains/index.blade.php
@@ -75,7 +75,7 @@
 
         <!-- Modal para crear una noticia -->
         <div class="modal fade" id="modal-default"  aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h4 class="modal-title">Nueva Noticia</h4>
@@ -88,54 +88,55 @@
                         @csrf
                         @method('POST')
                         <div class="modal-body">
-                            <img src="{{asset('img/bingo.jpg')}}" id="imagenSeleccionada" class="card-img-top img-fluid" width="17px" height="27px">
-
-
-
-
-
-
-
-                            <div style="max-height: 365px; overflow-y: scroll; overflow-x: hidden">
-                                <div class="d-flex justify-content-end">
-                                    <span class="text-danger mt-1">* </span><span>Campo requerido.</span>
-                                </div>
-                                <div class="form-group">
-                                    <label for="title"><span class="text-danger">*</span> Título:</label>
-                                    <input type="text" name="title" required class="form-control form-control-border" id="title" placeholder="Título">
-                                </div>
-                                <div class="row">
-                                    <div class="col-12 col-md-6">
-                                        <div class="form-group">
-                                            <label for="imagen"><span class="text-danger">*</span> Imagen:</label>
-                                            <input type="file" name="imagen" required class="form-control form-control-border" id="imagen">
-                                        </div>
-                                    </div>
-                                    <div class="col-12 col-md-6">
-                                        <div class="form-group">
-                                            <label for="mas_info">Más información:</label>
-                                            <input type="url" name="mas_info" class="form-control form-control-border" id="mas_info" placeholder="Escriba la URL">
-                                        </div>
-                                    </div>
-                                    <div class="col-12 col-md-12">
-                                        <div class="form-group">
-                                            <label for="state_id">Estado:</label>
-                                            <select class="custom-select form-control-border" name="state_id" id="state_id">
-                                                @foreach($states as $state)
-                                                    <option value="{{$state->id}}">{{$state->name}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-
-
+                            <div class="row">
+                                <div class="col-3">
+                                    <img src="{{asset('img/bingo.jpg')}}" id="imagenSeleccionada" class="card-img-top img-fluid" width="17px" height="27px">
 
                                 </div>
-                                <div class="form-group">
-                                    <label for="description"><span class="text-danger">*</span> Descripción</label>
-                                    <textarea id="compose-textarea" name="description" required class="form-control" style="height: 500px!important;">
+                                <div class="col-9">
+                                    <div style="max-height: 365px; overflow-y: scroll; overflow-x: hidden">
+                                        <div class="d-flex justify-content-end">
+                                            <span class="text-danger mt-1">* </span><span>Campo requerido.</span>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="title"><span class="text-danger">*</span> Título:</label>
+                                            <input type="text" name="title" required class="form-control form-control-border" id="title" placeholder="Título">
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-12 col-md-6">
+                                                <div class="form-group">
+                                                    <label for="imagen"><span class="text-danger">*</span> Imagen:</label>
+                                                    <input type="file" name="imagen" required class="form-control form-control-border" id="imagen">
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-6">
+                                                <div class="form-group">
+                                                    <label for="mas_info">Más información:</label>
+                                                    <input type="url" name="mas_info" class="form-control form-control-border" id="mas_info" placeholder="Escriba la URL">
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-12">
+                                                <div class="form-group">
+                                                    <label for="state_id">Estado:</label>
+                                                    <select class="custom-select form-control-border" name="state_id" id="state_id">
+                                                        @foreach($states as $state)
+                                                            <option value="{{$state->id}}">{{$state->name}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+
+
+
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="description"><span class="text-danger">*</span> Descripción</label>
+                                            <textarea id="compose-textarea" name="description" required class="form-control" style="height: 500px!important;">
 
                                     </textarea>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
@@ -153,7 +154,7 @@
         @foreach($cardmains as $cardmain)
             <!-- Modal para editar una noticia -->
             <div class="modal fade" id="modal-edit-noticia_{{$loop->iteration}}"  aria-hidden="true">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h4 class="modal-title">Editar Notica</h4>
@@ -166,44 +167,52 @@
                             @csrf
                             @method('PUT')
                             <div class="modal-body">
-                                <div style="max-height: 365px; overflow-y: scroll; overflow-x: hidden">
-                                    <div class="d-flex justify-content-end">
-                                        <span class="text-danger mt-1">* </span><span>Campo requerido.</span>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="title"><span class="text-danger">*</span> Título:</label>
-                                        <input type="text" value="{{$cardmain->title}}" name="title" required class="form-control form-control-border" id="title" placeholder="Título">
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-12 col-md-6">
-                                            <div class="form-group">
-                                                <label for="imagen"><span class="text-danger">*</span> Imagen:</label>
-                                                <input type="file" value="{{$cardmain->imagen}}" name="imagen"  class="form-control form-control-border" id="imagen">
-                                            </div>
-                                        </div>
-                                        <div class="col-12 col-md-6">
-                                            <div class="form-group">
-                                                <label for="mas_info">Más información:</label>
-                                                <input type="url" value="{{$cardmain->mas_info}}" name="mas_info" class="form-control form-control-border" id="mas_info" placeholder="Escriba la URL">
-                                            </div>
-                                        </div>
-                                        <div class="col-12 col-md-12">
-                                            <div class="form-group">
-                                                <label for="state_id">Estado:</label>
-                                                <select class="custom-select form-control-border" name="state_id" id="state_id">
-                                                    @foreach($states as $state)
-                                                        <option value="{{$state->id}}" {{ $state->id == $cardmain->state_id ? 'selected' : '' }} {{ old('state_id') == $state->id ? 'selected' : '' }}>{{$state->name}}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
 
+                                <div class="row">
+                                    <div class="col-3">
+                                        <img src="{{asset('storage/' . $cardmain->imagen)}}" id="imagenSeleccionadas_{{$loop->iteration}}" class="card-img-top img-fluid" width="17px" height="27px">
                                     </div>
-                                    <div class="form-group">
-                                        <label for="description"><span class="text-danger">*</span> Descripción</label>
-                                        <textarea id="editNovedad_{{$loop->iteration}}" name="description" required class="form-control" style="height: 500px!important;">
+                                    <div class="col-9">
+                                        <div style="max-height: 365px; overflow-y: scroll; overflow-x: hidden">
+                                            <div class="d-flex justify-content-end">
+                                                <span class="text-danger mt-1">* </span><span>Campo requerido.</span>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="title"><span class="text-danger">*</span> Título:</label>
+                                                <input type="text" value="{{$cardmain->title}}" name="title" required class="form-control form-control-border" id="title" placeholder="Título">
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-12 col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="images_{{$loop->iteration}}"><span class="text-danger">*</span> Imagen:</label>
+                                                        <input type="file" value="{{$cardmain->imagen}}" name="imagen"  class="form-control form-control-border" id="images_{{$loop->iteration}}">
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="mas_info">Más información:</label>
+                                                        <input type="url" value="{{$cardmain->mas_info}}" name="mas_info" class="form-control form-control-border" id="mas_info" placeholder="Escriba la URL">
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="state_id">Estado:</label>
+                                                        <select class="custom-select form-control-border" name="state_id" id="state_id">
+                                                            @foreach($states as $state)
+                                                                <option value="{{$state->id}}" {{ $state->id == $cardmain->state_id ? 'selected' : '' }} {{ old('state_id') == $state->id ? 'selected' : '' }}>{{$state->name}}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="description"><span class="text-danger">*</span> Descripción</label>
+                                                <textarea id="editNovedad_{{$loop->iteration}}" name="description" required class="form-control" style="height: 500px!important;">
                                             {!! $cardmain->description !!}
                                         </textarea>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -263,5 +272,19 @@
                 reader.readAsDataURL(this.files[0]);
             });
         });
+
+    </script>
+    <script>
+        @foreach($cardmains as $cardmain)
+        $(document).ready(function (e) {
+            $('#images_{{$loop->iteration}}').change(function(){
+                let reader = new FileReader();
+                reader.onload = (e) => {
+                    $('#imagenSeleccionadas_{{$loop->iteration}}').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(this.files[0]);
+            });
+        });
+        @endforeach
     </script>
 @endsection
